68229420d93724c5926166ea57ac5d6acd767095
CalleAlexanderson <calle.alexanderson@telia.com>
2024-04-15 18:01:01 +0200

    Lagt till put, fixat så anropen funkar överallt och inte bara min webbplats

--- server.js
+++ server.js
@@ -49,36 +49,83 @@ app.post("/api/workplaces", (req, res) => {
 
     const queryStmt = "INSERT INTO work (companyname, jobtitle, location, description) VALUES ?"
     // tar de värden som skickats med i body på fetch anropet 
-    let companyname = req.body.companyname
-    let jobtitle = req.body.jobtitle
-    let location = req.body.location
-    let description = req.body.description
+    let body = req.body;
 
-    // skapar värdena till den nya jobbposten
-    let newWork = [
-        [companyname,jobtitle,location,description]
-    ]
+    let companyname = body[0].companyname
+    let jobtitle = body[0].jobtitle
+    let location = body[0].location
+    let description = body[0].description
 
-    connection.query(queryStmt, [newWork], (err, results) => {
-        if (err) throw err;
+    if (companyname != "" && jobtitle != "" && location != "" && description != "") {
+        // skapar värdena till den nya jobbposten
+        let newWork = [
+            [companyname, jobtitle, location, description]
+        ]
 
-        console.table(results)
-    });
+        connection.query(queryStmt, [newWork], (err, results) => {
+            if (err) throw err;
 
-    // skickar tillbaka detta meddelande till fetch anropet
-    res.json({
-        message: "workplace added",
-        companyname: req.body.companyname,
-        jobtitle: req.body.jobtitle,
-        location: req.body.location,
-        description: req.body.description
-    });
+            console.table(results)
+        });
+
+        // skickar tillbaka detta meddelande till fetch anropet
+        res.json({
+            message: "workplace added",
+            companyname: req.body.companyname,
+            jobtitle: req.body.jobtitle,
+            location: req.body.location,
+            description: req.body.description
+        });
+    } else {
+        res.json({
+            message: "All fields must be filled to add a new workplace"
+        });
+    }
 });
 
-// app.put("/api/workplaces/:id", (req, res) => {
-//     const queryStmt = "UPDATE work WHERE ID = ?"
-//     res.json({ message: "user updated: " + req.params.id });
-// });
+app.put("/api/workplaces/:id", (req, res) => {
+    const queryStmt = "UPDATE work SET companyname = ?, jobtitle = ?, location = ?, description = ? WHERE ID = ?"
+    let id = req.params.id;
+
+    let body = req.body;
+
+    let companyname = body[0].companyname
+    let jobtitle = body[0].jobtitle
+    let location = body[0].location
+    let description = body[0].description
+
+    if (companyname != "" && jobtitle != "" && location != "" && description != "") {
+        let uppdatedcompanyname = [
+            [companyname]
+        ]
+
+        let uppdatedjobtitle = [
+            [jobtitle]
+        ]
+
+        let uppdatedlocation = [
+            [location]
+        ]
+
+        let uppdateddescription = [
+            [description]
+        ]
+
+        connection.query(queryStmt, [uppdatedcompanyname, uppdatedjobtitle, uppdatedlocation, uppdateddescription, id], (err) => {
+            if (err) {
+                console.log(err.message);
+            }
+        })
+
+        res.json({
+            message: "user updated: " + req.params.id
+        });
+    } else {
+        res.json({
+            message: "All fields must be filled to update a workplace"
+        });
+    }
+});
 
 app.delete("/api/workplaces/:id", (req, res) => {
     // skapar den parameter som skickas med i query genom att ta en parameter från url som används vid fetch anropet
@@ -88,7 +135,7 @@ app.delete("/api/workplaces/:id", (req, res) => {
             console.log(err.message);
         }
     })
-    res.json({ message: "workplace with ID " + req.params.id + " deleted"});
+    res.json({ message: "workplace with ID " + req.params.id + " deleted" });
 });
 
 app.listen(port, () => {
